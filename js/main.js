3a48d2b175dffb36b2c50547935dcdc3059fce5c
Malnev.Dmitriy <malnev.dmitriy@wildberrises.ru>
2018-11-14 11:41:10 +0300

    Исправляет замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 'use strict';
 
-(() => {
-  // Инициализация счетчика и шаблона
-  const INITIAL_SCREEN_ID = 0;
-  const SLIDE_NAVIGATION_CONTROLS =
+// Инициализация счетчика и шаблона
+const INITIAL_SCREEN_ID = 0;
+const SLIDE_NAVIGATION_CONTROLS =
   `<div class="arrows__wrap">
   <style>
     .arrows__wrap {
@@ -22,66 +21,58 @@
   <button class="arrows__btn"><-</button>
   <button class="arrows__btn">-></button>
 </div>`;
-  // Кнопки влево и вправо
-  const KEY_LEFT = 37;
-  const KEY_RIGHT = 39;
-  // Очищаем DOM
-  const clearElement = (elem) => {
-    while (elem.firstChild) {
-      elem.removeChild(elem.firstChild);
-    }
-  };
-  // Находим все шаблоны
-  const gameField = document.querySelector(`#main`);
-  const screens = [
-    `intro`,
-    `greeting`,
-    `rules`,
-    `game-1`,
-    `game-2`,
-    `game-3`,
-    `stats`
-  ].map((id) => document.querySelector(`#${id}`));
-  let currentScreen = INITIAL_SCREEN_ID;
-  // Показываем экран
-  const showScreen = (number) => {
-    currentScreen = Math.min(screens.length - 1, Math.max(0, number));
-    if (currentScreen === number) {
-      clearElement(gameField);
-      const currentFragment = screens[currentScreen].content.cloneNode(true);
-      gameField.appendChild(currentFragment);
-    }
-  };
-  // Создаем контролы
-  const createNavigationControls = () => {
-    const fragment = document.createRange().createContextualFragment(SLIDE_NAVIGATION_CONTROLS);
-    document.body.appendChild(fragment);
-  };
-  // Создаем обработчики для кнопок
-  const createNavigationHandlers = () => {
-    const arrowButtons = document.querySelectorAll(`.arrows__btn`);
-    arrowButtons[0].addEventListener(`click`, previousSlideHandler);
-    arrowButtons[1].addEventListener(`click`, nextSlideHandler);
-    document.addEventListener(`keydown`, arrowKeyDownHandler);
-  };
-  // Обработчики назад и вперед
-  const previousSlideHandler = () => {
-    showScreen(currentScreen - 1);
-  };
+// Находим все шаблоны
+const gameField = document.querySelector(`#main`);
+const screens = [
+  `intro`,
+  `greeting`,
+  `rules`,
+  `game-1`,
+  `game-2`,
+  `game-3`,
+].map((id) => document.querySelector(`#${id}`));
+let currentScreen = INITIAL_SCREEN_ID;
+// Показываем экран
+const showScreen = (number) => {
+  currentScreen = Math.min(screens.length - 1, Math.max(0, number));
+  if (currentScreen === number) {
+    gameField.innerHTML = ``;
+    const currentFragment = screens[currentScreen].content.cloneNode(true);
+    gameField.appendChild(currentFragment);
+  }
+};
+// Создаем контролы
+const createNavigationControls = () => {
+  const fragment = document.createRange().createContextualFragment(SLIDE_NAVIGATION_CONTROLS);
+  document.body.appendChild(fragment);
+};
+// Создаем обработчики для кнопок
+const createNavigationHandlers = () => {
+  const [leftButton, rightButton] = document.querySelectorAll(`.arrows__btn`);
+  leftButton.addEventListener(`click`, previousSlideHandler);
+  rightButton.addEventListener(`click`, nextSlideHandler);
+  document.addEventListener(`keydown`, arrowKeyDownHandler);
+};
+// Обработчики назад и вперед
+const previousSlideHandler = () => {
+  showScreen(currentScreen - 1);
+};
 
-  const nextSlideHandler = () => {
-    showScreen(currentScreen + 1);
-  };
-  // Действия по клику на кнопки влево и вправо
-  const arrowKeyDownHandler = (evt) => {
-    if (evt.keyCode === KEY_LEFT) {
+const nextSlideHandler = () => {
+  showScreen(currentScreen + 1);
+};
+// Действия по клику на кнопки влево и вправо
+const arrowKeyDownHandler = (evt) => {
+  switch (evt.key) {
+    case `ArrowLeft`:
       showScreen(currentScreen - 1);
-    } else if (evt.keyCode === KEY_RIGHT) {
+      break;
+    case `ArrowRight`:
       showScreen(currentScreen + 1);
-    }
-  };
-  // Запуск приложения
-  createNavigationControls();
-  createNavigationHandlers();
-  showScreen(currentScreen);
-})();
+      break;
+  }
+};
+// Запуск приложения
+createNavigationControls();
+createNavigationHandlers();
+showScreen(currentScreen);
